33dc9a5c40dae36e42c4e30a7a73470c840a60d8
coazit <coazit@gmail.com>
2021-02-16 00:02:09 -0500

    ViewChild BusquedaComponent

---
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -0,0 +1,21 @@
+import { ThrowStmt } from '@angular/compiler';
+import { Component, ElementRef, ViewChild} from '@angular/core';
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html',
+  styles: [
+  ]
+})
+export class BusquedaComponent{
+
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
+
+  buscar() {
+    const valor = this.txtBuscar.nativeElement.value;
+    console.log(valor);
+    this.txtBuscar.nativeElement.value = ''
+    
+  }  
+
+}
---
+++ src/app/gifs/gifs-page/gifs-page.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gifs-page',
+  templateUrl: './gifs-page.component.html',
+})
+export class GifsPageComponent {
+
+
+}
