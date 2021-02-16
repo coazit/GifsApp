684724c2b99e8a660dcb1276e062524c17b917df
coazit <coazit@gmail.com>
2021-02-16 00:42:17 -0500

    Gifsservice

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,5 +1,6 @@
 import { ThrowStmt } from '@angular/compiler';
 import { Component, ElementRef, ViewChild} from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-busqueda',
@@ -11,9 +12,11 @@ export class BusquedaComponent{
 
   @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
 
+  constructor(private gifsService: GifsService) {}
+
   buscar() {
     const valor = this.txtBuscar.nativeElement.value;
-    console.log(valor);
+    this.gifsService.buscarGifs( valor );
     this.txtBuscar.nativeElement.value = ''
     
   }  
