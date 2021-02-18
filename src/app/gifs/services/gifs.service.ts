8d9ca9991ce5c2567583747f6b81c2cee31fe570
coazit <coazit@gmail.com>
2021-02-18 14:04:39 -0500

    Tipado de gifs.interface

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { SearchGifsResponse, Gif } from '../interface/gifs.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ export class GifsService {
   private _historial: string[] = [];
 
   //TODO: cambiar any por su tipo correspondiente
-  public resultados: any [] = [];
+  public resultados: Gif [] = [];
 
   get historial() {    
     return [...this._historial];
@@ -24,10 +25,10 @@ export class GifsService {
       this._historial  = this.historial.splice(0,10);
     }  
     
-    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=BGVQdyPTzjXIm3IOmxEMVEoMPNOOCcEX&q=${ query }&limit=10`)
-      .subscribe(( resp: any ) => {
+    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=BGVQdyPTzjXIm3IOmxEMVEoMPNOOCcEX&q=${ query }&limit=10`)
+      .subscribe(( resp ) => {
         console.log( resp.data );
-        this.resultados = resp.data;
+        this.resultados = resp.data;                
       });  
   }
 }
