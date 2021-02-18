06fbd06aa89d7596a36ffafa6e515dcf24b7d127
coazit <coazit@gmail.com>
2021-02-18 13:42:37 -0500

    Utilizando Http

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -8,6 +8,9 @@ export class GifsService {
   private apiKey = 'BGVQdyPTzjXIm3IOmxEMVEoMPNOOCcEX';
   private _historial: string[] = [];
 
+  //TODO: cambiar any por su tipo correspondiente
+  public resultados: any [] = [];
+
   get historial() {    
     return [...this._historial];
   }
@@ -21,10 +24,10 @@ export class GifsService {
       this._historial  = this.historial.splice(0,10);
     }  
     
-    this.http.get('https://api.giphy.com/v1/gifs/search?api_key=BGVQdyPTzjXIm3IOmxEMVEoMPNOOCcEX&q=dragon ball z&limit=10')
+    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=BGVQdyPTzjXIm3IOmxEMVEoMPNOOCcEX&q=${ query }&limit=10`)
       .subscribe(( resp: any ) => {
         console.log( resp.data );
-      })
-  
+        this.resultados = resp.data;
+      });  
   }
 }
