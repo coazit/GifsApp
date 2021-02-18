bd0723374dd01bb061afc592d53daa2446ecb68d
coazit <coazit@gmail.com>
2021-02-18 15:04:08 -0500

    Cargando imagenes automaticamente - localStorage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -17,7 +17,8 @@ export class GifsService {
   }
 
   constructor( private http: HttpClient ){
-    this._historial = JSON.parse( localStorage.getItem('historial')! ) || [];    
+    this._historial = JSON.parse( localStorage.getItem('historial')! ) || [];  
+    this.resultados = JSON.parse( localStorage.getItem('resultados')! ) || [];   
   }
 
   buscarGifs( query:string ) {
@@ -26,13 +27,14 @@ export class GifsService {
       this._historial.unshift(query);
       this._historial  = this.historial.splice(0,10);
 
-      localStorage.setItem('historial', JSON.stringify( this._historial ));
+      localStorage.setItem('historial', JSON.stringify( this._historial ));      
     }  
     
     this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=BGVQdyPTzjXIm3IOmxEMVEoMPNOOCcEX&q=${ query }&limit=10`)
       .subscribe(( resp ) => {
         console.log( resp.data );
-        this.resultados = resp.data;                
+        this.resultados = resp.data;
+        localStorage.setItem('resultados', JSON.stringify( this.resultados ));                
       });  
   }
 }
