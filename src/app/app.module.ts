8c3767f41ef154f40dc7050ccb2db6860779fdb6
coazit <coazit@gmail.com>
2021-02-18 12:21:23 -0500

    HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from "@angular/common/http";
+
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { GifsModule } from './gifs/gifs.module';
@@ -11,6 +13,7 @@ import { GifsModule } from './gifs/gifs.module';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     SharedModule,
     GifsModule
   ],
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -11,15 +12,19 @@ export class GifsService {
     return [...this._historial];
   }
 
+  constructor( private http: HttpClient ){}
+
   buscarGifs( query:string ) {
     query = query.trim().toLocaleLowerCase();
     if( !this.historial.includes(query)) {      
       this._historial.unshift(query);
       this._historial  = this.historial.splice(0,10);
-    }    
-    console.log(this._historial);
+    }  
     
+    this.http.get('https://api.giphy.com/v1/gifs/search?api_key=BGVQdyPTzjXIm3IOmxEMVEoMPNOOCcEX&q=dragon ball z&limit=10')
+      .subscribe(( resp: any ) => {
+        console.log( resp.data );
+      })
+  
   }
-
-
 }
